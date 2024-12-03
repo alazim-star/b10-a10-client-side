0c8262bd3352dd210916dae1470ac322e695e103
unknown <alazim-star>
2024-12-03 17:36:33 +0600

    install react rowter

---
+++ FireBase/fireBase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBs_jYwWKmWH2ftSydCVBFS5BO31iSTD5U",
+  authDomain: "b10-a10-client-side.firebaseapp.com",
+  projectId: "b10-a10-client-side",
+  storageBucket: "b10-a10-client-side.firebasestorage.app",
+  messagingSenderId: "313731626683",
+  appId: "1:313731626683:web:61eca658895559a1d29b9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
